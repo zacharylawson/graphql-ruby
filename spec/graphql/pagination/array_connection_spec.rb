9ba5b6786d74b57e56a081efe70e414efb81cfd3
Robert Mosolgo <rdmosolgo@gmail.com>
2020-07-11 10:32:01 -0400

    Add a basic test for eight-character cursors

--- spec/graphql/pagination/array_connection_spec.rb
+++ spec/graphql/pagination/array_connection_spec.rb
@@ -19,4 +19,24 @@ describe GraphQL::Pagination::ArrayConnection do
   }
 
   include ConnectionAssertions
+
+  class EightCharacterTestSchema < GraphQL::Schema
+    class Query < GraphQL::Schema::Object
+      field :items, GraphQL::Types::String.connection_type, null: false
+
+      def items
+        ["A", "B", "C", "D", "E"]
+      end
+    end
+
+    query(Query)
+    use GraphQL::Execution::Interpreter
+    use GraphQL::Analysis::AST
+    use GraphQL::Pagination::Connections
+  end
+
+  it "works with eight-character cursors" do
+    res = EightCharacterTestSchema.execute("{ items(first: 3, after: \"aaaaaaaa\") { edges { node } } }")
+    assert_equal ["A", "B", "C"], res["data"]["items"]["edges"].map { |e| e["node"]}
+  end
 end
